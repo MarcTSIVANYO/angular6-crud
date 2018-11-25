0031ce651ae43274848748653b0dcc51b141b6a9
Esinniobiwa Quareeb <ayinde@codesquad.co>
2018-11-25 04:01:09 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ReadProductsComponent } from './read-products/read-products.component';
+import { CreateProductComponent } from './create-product/create-product.component';
+import { ReadOneProductComponent } from './read-one-product/read-one-product.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
+import { DeleteProductComponent } from './delete-product/delete-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReadProductsComponent,
+    CreateProductComponent,
+    ReadOneProductComponent,
+    UpdateProductComponent,
+    DeleteProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    ReactiveFormsModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
